e6d0662c48106f20c6d82aa53bc1c806e9a0ac49
Abdumalik <abdumalikcodes7753@gmail.com>
2022-10-20 17:45:16 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function cardNumber(cardPin){
+    if (cardPin.length < 16 || cardPin.length > 16 || isNaN(+cardPin)){
+        console.log('Error writing');
+    }
+    if(cardPin.startsWith('8600')){
+        console.log('Your card is Uzcard!');
+    }else if (cardPin.startsWith('9680')){
+        console.log('Your card is Khumo!');
+    }else{
+        console.log('This is an error!');
+    }
+    let symbol = "**********";
+    console.log(cardPin.slice(0 , 4) + symbol + cardPin.slice(12 , 16));
+}
+cardNumber('9680123456798875')
